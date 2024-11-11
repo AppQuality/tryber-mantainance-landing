db7f0ae0a416cf1bdb89e85b686117a84a682a15
Davide Bizzi <davide.bizzi@unguess.io>
2024-11-11 12:50:40 +0100

    ci: Use url for favicon

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 printenv
 find /usr/share/nginx/html/index.html -type f -exec sed -i 's/%SITE_NAME%/'$SITE_NAME'/g' {} +
+find /usr/share/nginx/html/index.html -type f -exec sed -i 's#%FAVICON_URL%#'$FAVICON_URL'#g' {} +
 find /usr/share/nginx/html/css -type f -exec sed -i 's/%GRADIENT_BACKGROUND%/'$GRADIENT_BACKGROUND'/g' {} +
 
 nginx -g "daemon off;"
